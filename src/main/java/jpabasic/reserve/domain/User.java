a38adf686500c9da47076ef76d10a5b6ba3b42e0
yngjn963 <yngjn963@naver.com>
2023-06-01 21:59:10 +0900

    feat: JPA & Spring Data JPA 기초
    섹션 0. JPA 기본 매핑
    JPA 기초 01 JPA 해보기

---
+++ src/main/java/jpabasic/reserve/domain/User.java
@@ -0,0 +1,49 @@
+package jpabasic.reserve.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity // DB 테이블과 매핑 대상
+@Table(name = "user") // user 테이블과 매핑
+public class User {
+    @Id
+    private String email;
+    private String name;
+    @Column(name = "create_date")
+    private LocalDateTime createDate;
+
+    protected User() {
+
+    }
+
+    public User(String email, String name, LocalDateTime createDate) {
+        this.email = email;
+        this.name = name;
+        this.createDate = createDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+}
